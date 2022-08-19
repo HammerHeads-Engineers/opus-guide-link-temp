0a0c0a7e549fdfb287572dc440a6d6acff1cefc3
av <av@pop-os.localdomain>
2022-08-19 09:34:44 +0300

    Flask tweaks

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
+#@app.route('/')
+#def hello():
+#    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('index.html'), 404
 
 @app.route('/<link_uid>/')
-def capitalize(link_uid):
+def show_instruction(link_uid):
     return render_template('index.html', link_uid=link_uid)
+
+@app.route('/preview/<link_uid>')
+def preview_instruction(link_uid):
+	return render_template('index.html', link_uid=link_uid)
